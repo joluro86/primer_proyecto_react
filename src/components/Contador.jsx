c221cbc04020b4571a02ca1770e3eddd38088f0e
joluro86 <joluro86@hotmail.com>
2022-11-09 21:46:03 -0500

    inicio cronometro

--- src/components/Contador.jsx
+++ src/components/Contador.jsx
@@ -1,40 +1,22 @@
 import React from 'react'
-import { useStopwatch } from 'react-timer-hook';
 
 const Contador = () => {
 
-    const {
-        seconds,
-        minutes,
-        hours,
-        days,
-        isRunning,
-        start,
-        pause,
-        reset,
-    } = useStopwatch({ autoStart: false });
-
     const [contador, SetContador] = React.useState(0)
 
-    const aumentar = () => {
+    const aumentar = setInterval(() => {
         SetContador(contador + 1)
-        if (contador === 10) {
+        if(contador===10){
             SetContador(0)
-        }
-    }
+        } 
+        console.log(contador)
+    }, 1000)
+
 
     return (
         <div>
             <h2>Contador</h2>
             <h3>Nuestro numero aumentando: {contador}</h3>
-            <button onClick={() => aumentar()}>Aumentar</button>
-
-            <span>{days}</span>:<span>{hours}</span>:<span>{minutes}</span>:<span>{seconds}</span>
-            <p>{isRunning ? 'Running' : 'Not running'}</p>
-            <button onClick={start}>Start</button>
-            <button onClick={pause}>Pause</button>
-            <button onClick={reset}>Reset</button>
-
         </div>
     )
 }
